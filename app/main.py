9c1e2573ef644b9ff9912f341054f30f41042a19
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-04-15 21:03:31 +0300

    fix: fix import

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
-from core.init_db import create_first_superuser
+from app.core.init_db import create_first_superuser
 
 app = FastAPI(
     title=settings.app_title,
