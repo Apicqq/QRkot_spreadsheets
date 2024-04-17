2c35c9fac5ac582d97317bd8b43ed882c2d9e8e1
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-04-17 10:20:39 +0300

    feat: create custom exceptions

---
+++ app/core/exceptions.py
@@ -0,0 +1,12 @@
+class MaxRowsLimitExceeded(Exception):
+    """Raised when created spreadsheet would exceed technical limits of Google
+    spreadsheet.
+    """
+    pass
+
+
+class MaxColumnsLimitExceeded(Exception):
+    """Raised when created spreadsheet would exceed technical limits of Google
+    spreadsheet.
+    """
+    pass
