9d27af749f676ef97c0591bfa5a1f647a74fa891
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-04-17 12:16:14 +0300

    refactor: add code cleanup, no new functionality added

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -3,7 +3,10 @@ from datetime import datetime, timedelta
 from aiogoogle import Aiogoogle
 
 from app.core.config import settings
-from app.core.constants import UtilityConstants as Uconst
+from app.core.constants import (
+    UtilityConstants as Uconst,
+    ErrConstants as Errconst,
+)
 from app.core.exceptions import MaxColumnsLimitExceeded, MaxRowsLimitExceeded
 
 SPREADSHEET_DT_FORMAT = "%Y/%m/%d %H:%M:%S"
@@ -37,28 +40,31 @@ def get_spreadsheet_body(_datetime: str, rows: int, columns: int = 3) -> dict:
 
 
 async def create_spreadsheet(
-        wrapper_service: Aiogoogle, spreadsheet: list
+        wrapper_service: Aiogoogle, projects: list
 ) -> tuple[str, str]:
     service = await wrapper_service.discover("sheets", "v4")
     spreadsheet_body = get_spreadsheet_body(
         datetime.now().strftime(SPREADSHEET_DT_FORMAT),
-        rows=len(spreadsheet) + Uconst.HEADER_ROWS_COUNT,
+        rows=len(projects) + len(get_spreadsheet_header("")),
+        columns=max(map(len, get_spreadsheet_header(""))),
     )
     grid_properties = spreadsheet_body["sheets"][0]["properties"][
         "gridProperties"
     ]
     if grid_properties["rowCount"] >= Uconst.GOOGLE_SPREADSHEET_ROWS_LIMIT:
         raise MaxRowsLimitExceeded(
-            f"rows {grid_properties['rowCount']}/"
-            f"{Uconst.GOOGLE_SPREADSHEET_ROWS_LIMIT}"
+            Errconst.EXCEEDED_ROWS_AMOUNT.format(
+                grid_properties["rowCount"],
+            )
         )
     if (
             grid_properties["columnCount"] >=
             Uconst.GOOGLE_SPREADSHEET_COLUMNS_LIMIT
     ):
         raise MaxColumnsLimitExceeded(
-            f"columns {grid_properties['columnCount']}/"
-            f"{Uconst.GOOGLE_SPREADSHEET_COLUMNS_LIMIT}"
+            Errconst.EXCEEDED_COLUMNS_AMOUNT.format(
+                grid_properties["columnCount"],
+            )
         )
     response = await wrapper_service.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
@@ -83,7 +89,7 @@ async def set_user_permissions(
 
 
 async def spreadsheet_update_value(
-        spreadsheet_id: str, wrapper_service: Aiogoogle, charity_projects: list
+        spreadsheet_id: str, wrapper_service: Aiogoogle, projects: list
 ) -> None:
     service = await wrapper_service.discover("sheets", "v4")
     table_values = [
@@ -92,7 +98,7 @@ async def spreadsheet_update_value(
         ),
         *[
             list(map(str, [title, timedelta(days=rate), description]))
-            for title, rate, description in charity_projects
+            for title, rate, description in projects
         ],
     ]
     update_body = {"values": table_values, "majorDimension": "ROWS"}
@@ -100,8 +106,8 @@ async def spreadsheet_update_value(
         service.spreadsheets.values.update(
             spreadsheetId=spreadsheet_id,
             range=f"R1C1:"
-                  f"R{len(update_body.get('values'))}"
-                  f"C{max(len(row) for row in update_body.get('values'))}",
+                  f"R{len(table_values)}"
+                  f"C{max(map(len, table_values))}",
             valueInputOption="USER_ENTERED",
             json=update_body,
         )
