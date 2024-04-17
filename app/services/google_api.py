4dee9f98888471cbecd3d126243f8824c705b688
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-04-17 10:22:56 +0300

    refactor: refactor create/update spreadsheet functions with custom error handling, computing range at runtime etc

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,12 +1,12 @@
 from datetime import datetime, timedelta
-from http import HTTPStatus
 
 from aiogoogle import Aiogoogle
-from fastapi import HTTPException
 
 from app.core.config import settings
-from app.core.constants import (ErrConstants as Econst,
-                                UtilityConstants as Uconst)
+from app.core.constants import (UtilityConstants as Uconst)
+from app.core.exceptions import (
+    MaxColumnsLimitExceeded, MaxRowsLimitExceeded
+)
 
 SPREADSHEET_DT_FORMAT = "%Y/%m/%d %H:%M:%S"
 
@@ -43,22 +43,36 @@ def get_spreadsheet_body(
 
 async def create_spreadsheet(
         wrapper_service: Aiogoogle,
-        rows: int
-) -> dict:
+        spreadsheet: list
+) -> tuple[str, str]:
     service = await wrapper_service.discover(
         "sheets", "v4"
     )
     spreadsheet_body = get_spreadsheet_body(
         datetime.now().strftime(SPREADSHEET_DT_FORMAT),
-        rows
+        rows=len(spreadsheet) + Uconst.HEADER_ROWS_COUNT
     )
+    grid_properties = spreadsheet_body["sheets"][0]["properties"][
+        "gridProperties"
+    ]
+    if grid_properties[
+        'rowCount'
+    ] >= Uconst.GOOGLE_SPREADSHEET_ROWS_LIMIT:
+        raise MaxRowsLimitExceeded(
+            f"rows {grid_properties['rowCount']}/"
+            f"{Uconst.GOOGLE_SPREADSHEET_ROWS_LIMIT}"
+        )
+    if grid_properties[
+        'columnCount'
+    ] >= Uconst.GOOGLE_SPREADSHEET_COLUMNS_LIMIT:
+        raise MaxColumnsLimitExceeded(
+            f"columns {grid_properties['columnCount']}/"
+            f"{Uconst.GOOGLE_SPREADSHEET_COLUMNS_LIMIT}"
+        )
     response = await wrapper_service.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
-    return dict(
-        id=response["spreadsheetId"],
-        url=response["spreadsheetUrl"]
-    )
+    return response["spreadsheetId"], response["spreadsheetUrl"]
 
 
 async def set_user_permissions(
@@ -94,22 +108,19 @@ async def spreadsheet_update_value(
         *get_spreadsheet_header(
             datetime.now().strftime(SPREADSHEET_DT_FORMAT)),
         *[list(
-            map(str, [project[0], timedelta(days=project[1]), project[2]])
-        ) for project in charity_projects]
+            map(str, [title, timedelta(days=rate), description])
+        ) for title, rate, description in charity_projects]
     ]
     update_body = {
         "values": table_values,
         "majorDimension": "ROWS"
     }
-    if len(update_body.get("values")) >= Uconst.GOOGLE_SPREADSHEET_ROWS_LIMIT:
-        raise HTTPException(
-            HTTPStatus.BAD_REQUEST,
-            Econst.PROJECTS_LIMIT_REACHED
-        )
     await wrapper_service.as_service_account(
         service.spreadsheets.values.update(
             spreadsheetId=spreadsheet_id,
-            range=f"R1C1:R{len(update_body.get('values'))}C3",
+            range=f"R1C1:"
+                  f"R{len(update_body.get('values'))}"
+                  f"C{max(len(row) for row in update_body.get('values'))}",
             valueInputOption="USER_ENTERED",
             json=update_body
         )
